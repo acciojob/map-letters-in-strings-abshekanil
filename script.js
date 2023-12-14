a00ad415ba33799d1d1f3bd5472502b1732b64fc
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-14 22:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+mapLetters(str){
+	let letterMap = {};
+	for(let i=0; i<str.length; i++)
+		{
+			let char = str[i];
+			if(char in letterMap)
+			{
+				letterMap[char].push(i);
+			}else
+			{
+				letterMap[char] = [i];
+			}
+		}
+	return letterMap;
+}
